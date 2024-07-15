4fe976b1897d0e550c2bd1e15a2d39e040c8bac7
Luke Lalor <lukehlalor@gmail.com>
2024-07-14 19:29:36 -0700

    add dummy key

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
+import os
+
 import pytest
 from pytest_asyncio import fixture
 from typedai import TypedAI
+
+# devs will want to use their key when recording new tests, but we want to allow running tests without a key set
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = "dummy_key"
 
 
 @pytest.fixture(scope="module")
